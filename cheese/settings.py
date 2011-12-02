3e2183bbf27166410e7c7744557ce1478f522f1c
Yiyang Tsui <edmoody822@gmail.com>
2011-12-01 16:43:51 -0800

    random user agent

--- cheese/settings.py
+++ cheese/settings.py
@@ -13,3 +13,15 @@ SPIDER_MODULES = ['cheese.spiders']
 NEWSPIDER_MODULE = 'cheese.spiders'
 USER_AGENT = '%s/%s' % (BOT_NAME, BOT_VERSION)
 
+#http://www.useragentstring.com/pages/useragentstring.php
+USER_AGENT_LIST = [
+        'Mozilla/5.0 (Macintosh; Intel Mac OS X 10.6; rv:9.0) Gecko/20100101 Firefox/9.0',
+        'Mozilla/5.0 (Windows NT 6.1; WOW64; rv:6.0a2) Gecko/20110613 Firefox/6.0a2',
+        'Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/535.7 (KHTML, like Gecko) Chrome/16.0.912.36 Safari/535.7'
+]
+
+DOWNLOADER_MIDDLEWARES = {
+    'cheese.random_user_agent.RandomUserAgentMiddleware': 400,
+    'scrapy.contrib.downloadermiddleware.useragent.UserAgentMiddleware': None,
+}
+
---
+++ cheese/spiders/ifryrice.py
@@ -0,0 +1,26 @@
+from scrapy.selector import HtmlXPathSelector
+from scrapy.contrib.linkextractors.sgml import SgmlLinkExtractor
+from scrapy.contrib.spiders import CrawlSpider, Rule
+from cheese.items import CheeseItem
+from scrapy.spider import BaseSpider
+
+class IfryriceSpider(CrawlSpider):
+    name = 'ifryrice'
+    allowed_domains = ['ifryrice.com']
+    start_urls = ['http://www.ifryrice.com/']
+
+    """
+    rules = (
+        Rule(SgmlLinkExtractor(allow=r'Items/'), callback='parse_item', follow=True),
+    )
+
+    def parse_item(self, response):
+        hxs = HtmlXPathSelector(response)
+        i = CheeseItem()
+        #i['domain_id'] = hxs.select('//input[@id="sid"]/@value').extract()
+        #i['name'] = hxs.select('//div[@id="name"]').extract()
+        #i['description'] = hxs.select('//div[@id="description"]').extract()
+        return i
+    """
+    def parse(self, response):
+        print response.body
