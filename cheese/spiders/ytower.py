128d54fd7a0f925faa5759af994221f48e92556c
yiyang <edmoody822@gmail.com>
2011-12-03 13:12:02 -0800

    ytower first level works

--- cheese/spiders/ytower.py
+++ cheese/spiders/ytower.py
@@ -5,17 +5,21 @@ from cheese.items import CheeseItem
 
 class YtowerSpider(CrawlSpider):
     name = 'ytower'
-    allowed_domains = ['ytower.com.tw']
-    start_urls = ['http://www.ytower.com.tw/']
+    #allowed_domains = ['ytower.com.tw']
+    start_urls = ['http://www.ytower.com.tw/recipe/iframe-search.asp']
 
     rules = (
-        Rule(SgmlLinkExtractor(allow=r'Items/'), callback='parse_item', follow=True),
+            #Rule(SgmlLinkExtractor(allow=r'/recipe-search2.asp?\.+'), callback='parse_item', follow=True),
+            Rule(SgmlLinkExtractor(allow=r'/recipe-search2.asp'), callback='parse_item', follow=True),
+            #Rule(SgmlLinkExtractor(restrict_xpaths='/html/body/table[2]/tbody/tr/td/table'), callback='parse_item', follow=False),
+            #Rule(SgmlLinkExtractor(), callback='parse_item', follow=True),
     )
 
     def parse_item(self, response):
-        hxs = HtmlXPathSelector(response)
-        i = CheeseItem()
+        #hxs = HtmlXPathSelector(response)
+        #i = CheeseItem()
+        print "r"*10, response.url
         #i['domain_id'] = hxs.select('//input[@id="sid"]/@value').extract()
         #i['name'] = hxs.select('//div[@id="name"]').extract()
         #i['description'] = hxs.select('//div[@id="description"]').extract()
-        return i
+        #return i
