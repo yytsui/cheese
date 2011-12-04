6b3d1aa66dbdbae9a9db9bf7da47c63a1d717847
yiyang <edmoody822@gmail.com>
2011-12-03 17:10:06 -0800

    refresh proxy list by time

--- cheese/random_user_agent.py
+++ cheese/random_user_agent.py
@@ -2,6 +2,7 @@ from cheese.user_agents_list import USER_AGENT_LIST
 import random
 from scrapy import log
 from lxml import etree
+import datetime
 
 def get_free_proxy():
     url =  "http://hidemyass.com/proxy-list/search-225783/"
@@ -20,15 +21,24 @@ def get_free_proxy():
 
 class RandomUserAgentProxyMiddleware(object):
     count = 0
+    period_start_time = datetime.datetime.now()
+    proxy_list = get_free_proxy()
+
     def process_request(self, request, spider):
         ua  = random.choice(USER_AGENT_LIST)
         if ua:
             request.headers.setdefault('User-Agent', ua)
-        if self.count % 200 == 0:
-            free_proxy = random.choice(get_free_proxy())
-            request.meta['proxy'] = free_proxy
-            log.msg("Change proxy to %s" % free_proxy, log.INFO)
-        self.count += 1
+        free_proxy = random.choice(self.proxy_list)
+        request.meta['proxy'] = free_proxy
+        log.msg("Change proxy to %s" % free_proxy, log.INFO)
+        self.refresh_proxy_list()
+
+    def refresh_proxy_list(self, minutes=10):
+        time_delta = datetime.datetime.now() - self.period_start_time
+        if time_delta.seconds > 10:
+            self.proxy_list = get_free_proxy()
+            self.period_start_time = datetime.datetime.now()
+
 
 if __name__ == '__main__':
     print get_free_proxy()
