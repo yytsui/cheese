062d1f378661f5684eb825b68b6449b0e1e5c927
yiyang <edmoody822@gmail.com>
2011-12-03 16:22:39 -0800

    get_free_proxy works

--- cheese/random_user_agent.py
+++ cheese/random_user_agent.py
@@ -2,6 +2,24 @@ from cheese.settings import FREE_PROXY_LIST
 from cheese.user_agents_list import USER_AGENT_LIST
 import random
 from scrapy import log
+import urllib
+from lxml import etree
+import StringIO
+
+def get_free_proxy():
+    url =  "http://hidemyass.com/proxy-list/search-225783/"
+    parser = etree.HTMLParser()
+    tree = etree.parse(url, parser)
+    #/html/body/div/div/table/tbody/tr[2]/td[2]/span
+    proxies = []
+    xpath = "id('listtable')/tr"
+    proxy = tree.xpath(xpath)
+    for p in proxy[:20]:
+        ip = p.find('td[2]/span').text
+        port = p.find('td[3]').text.strip()
+        http_proxy = 'http://%s:%s' % (ip, port)
+        proxies.append(http_proxy)
+    return proxies
 
 class RandomUserAgentMiddleware(object):
     
@@ -13,3 +31,6 @@ class RandomUserAgentMiddleware(object):
         request.meta['proxy'] = 'http://%s' % proxy
         #request.meta['proxy'] = 'http://64.90.59.115'
         #log.msg('>>>> UA %s'%request.headers)
+
+if __name__ == '__main__':
+    print get_free_proxy()
