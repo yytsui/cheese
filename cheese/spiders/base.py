b82dcbf8a94221b52b3f6570d41fbe5de91978d4
Yiyang Tsui <edmoody822@gmail.com>
2011-12-08 16:48:34 -0800

    implement base spider

---
+++ cheese/spiders/base.py
@@ -0,0 +1,75 @@
+from scrapy.selector import HtmlXPathSelector
+from scrapy.contrib.spiders import CrawlSpider
+from pprint import pprint
+
+class NotImplementError(Exception):
+    pass
+
+class BaseSpider(CrawlSpider):
+    name = 'base spider for interface purpose only'
+
+    def parse_list(self, response):
+        pass
+
+    def _on_receive_html(self, response):
+        self.url = response.url
+        self.hxs = HtmlXPathSelector(response)
+
+    @property
+    def title(self):
+        raise NotImplementError
+
+    @property
+    def main_picture(self):
+        raise NotImplementError
+
+    @property
+    def ingredients(self):
+        raise NotImplementError
+
+    @property
+    def steps(self):
+        raise NotImplementError
+
+    @property
+    def tags(self):
+        raise NotImplementError
+
+    @property
+    def author(self):
+        raise NotImplementError
+
+    @property
+    def view_count(self):
+        raise NotImplementError
+
+    @property
+    def fav_count(self):
+        raise NotImplementError
+
+    @property
+    def comments(self):
+        raise NotImplementError
+
+    @property
+    def misc_text(self):
+        raise NotImplementError
+
+    @property
+    def recipe(self):
+        return dict(title=self.title,
+                    main_picture=self.main_picture,
+                    ingredients=self.ingredients,
+                    steps=self.steps,
+                    tags=self.tags,
+                    author=self.author,
+                    view_count=self.view_count,
+                    fav_count=self.fav_count,
+                    comments=self.comments,
+                    misc_text=self.misc_text
+                )
+
+    def parse(self, response):
+        self._on_receive_html(response)
+        pprint(self.recipe)
+
